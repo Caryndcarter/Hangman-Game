66e2d05d36d14698610b335aaf776969058ae6ba
Caryn Carter <caryn.carter@northwestern.com>
2017-02-14 11:58:10 -0600

    added javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,57 +6,56 @@ var animalWords = [
 	"elephant",
 	"tiger",
 	"chimpanzee"
-	]
+	];
 
-var animal;
-var currentAnimal;
+var animal = "";
+var currentAnimal = "";
 var wins = 0;
 var losses = 0;
 var numGuessesLeft = 9;
 var lettersAlreadyGuessed =[];
-var letterGuess;
-var chosenAnimalLetters=[];
-var buildWord = [];
+var letterGuess = "";
+var currentAnimalLetters=[];
+var buildAnimal = [];
 
-/* 	General Steps:
-
-	1. set numGuessesLeft to 9
-	2. set lettersAlreadyGuessed array to blank/zero
-	3. choose a currentAnimal from the animalWords array at random 
-	4. make the currentAnimal an array of chosenAnimalLetters
-	5. display blanks for that animal's letters as buildWord array
-	6. take in letterGuess from user
-	7. compare letterGuess to letters in chosenAnimalLetters--get index number if identical
-	8. push letterGuess into index-number of buildWord array if correct OR 
-		push letterGuess into lettersAlreadyGuessed array and reduce numGuessesLeft by one
-	9. compare chosenAnimalLetters array to buildWord array and alert "You Win!" 
-		if correct, "You have no more guesses." if incorrect.
-	10. increment wins or losses accordingly 
-	11. if numGuessesLeft ===0, alert "You have no more guesses. Game over."
-	12. restart game if the user selects letters again
-
-
-Functions: 
-	1. startGame ();
+/*1. startGame ();
 		set numGuessesLeft to 9
 		set lettersAlreadyGuessed array to blank/zero
 		choose currentAnimal from the animalWords array at random
-		make the currentAnimal an array of chosenAnimalLetters
-		display blanks for currentAnimal's letters as buildWord array
+		make the currentAnimal an array of currentAnimalLetters
+		display blanks for currentAnimal's letters as buildAnimal array*/
 	
+
+function startGame (){
+	numGuessesLeft = 9;
+	lettersAlreadyGuessed = [];
+
+	currentAnimal = animalWords[(Math.floor(Math.random()* 6) +1)];
+		console.log(currentAnimal);
+
+	currentAnimalLetters = currentAnimal.split("");
+		console.log(currentAnimalLetters);
+
+	for (var i = 0; i < currentAnimal.length; i++) {
+		buildAnimal.push("_ ");
+	}
+		console.log(buildAnimal);
+		console.log(buildAnimal.join(" "));
+
+	// var blanks = document.getElementById("animalInProcess").innerHTML;
+	// blanks = buildAnimal.join(" ");
+	document.getElementById("animal").innerHTML = buildAnimal.join(" ");
+}
+startGame();
+
+/*
 	2. document.onkeyup (); takeInLetter
 		take in letterGuess from user and store it
-		compare letterGuess to letters in chosenAnimalLetters--get index number if identical
-		push letterGuess into index-number of buildWord array if correct OR 
-			push letterGuess into lettersAlreadyGuessed array and reduce numGuessesLeft by one
+		compare letterGuess to letters in currentAnimalLetters--get index number if identical
+		push letterGuess into index-number of buildAnimal array if correct OR 
+			push letterGuess into lettersAlreadyGuessed array and reduce numGuessesLeft by one */
 
-	3. compareWords ();
-		compare chosenAnimalLetters array to buildWord array and alert "You Win!" 
-			if correct, "You have no more guesses." if incorrect.
-		increment wins 
-		if numGuessesLeft ===0, alert "You have no more guesses. Game over." increment losses
 
-	4. restartGame ();
-		if numGuessesLeft === 0 OR chosenAnimalLetters === buildWord,
-		restart Game > startGame();
+
+
 
